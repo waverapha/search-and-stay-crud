7610224c5018f3347409fe5ce5cfa417c8314382
Rapha <raphael.v.work@gmail.com>
2023-03-10 15:38:06 -0300

    perf(nuxt.config.js): deliver less JS importing only necessary Bootstrap components

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,17 @@ export default {
     '@nuxtjs/axios',
   ],
 
+  bootstrapVue: {
+    components: ['BContainer', 'BRow', 'BCol', 'BForm', 'BFormGroup', 'BFormInvalidFeedback', 'BFormInput', 'BFormRadio', 'BButton', 'BAlert', 'BListGroup', 'BListGroupItem', 'BSkeleton', 'BPagination'],
+    componentPlugins: [
+      'LayoutPlugin',
+      'FormPlugin',
+      'FormInputPlugin',
+      'FormRadioPlugin'
+    ],
+    directivePlugins: []
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
